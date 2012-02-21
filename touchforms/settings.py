c937e597dd959d705cbcc9640f34b3cc995e6b9d
pld <peter@helioid.com>
2012-02-21 18:16:55 -0500

    add static file storage

--- touchforms/settings.py
+++ touchforms/settings.py
@@ -125,3 +125,5 @@ COUCHDB_DATABASE = 'http://%s%s/%s' % (
 COUCHDB_DATABASES = [('formplayer', COUCHDB_DATABASE)]
 
 REVISION = util.get_revision('git', ROOT_DIR, 'flag')
+
+STATICFILES_STORAGE = 'staticfiles.storage.StaticFilesStorage'
