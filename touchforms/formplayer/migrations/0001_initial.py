12ec903a074fd3a982d7dde186903c1204c21ab6
pld <peter@helioid.com>
2012-02-22 12:43:42 -0500

    fake migration

--- touchforms/formplayer/migrations/0001_initial.py
+++ touchforms/formplayer/migrations/0001_initial.py
@@ -7,26 +7,8 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
-        # Adding model 'XForm'
-        db.create_table('formplayer_xform', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.utcnow)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('namespace', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('version', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('uiversion', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('checksum', self.gf('django.db.models.fields.CharField')(max_length=40, blank=True)),
-            ('file', self.gf('django.db.models.fields.files.FileField')(max_length=255)),
-        ))
-        db.send_create_signal('formplayer', ['XForm'])
-
 
     def backwards(self, orm):
-        
-        # Deleting model 'XForm'
-        db.delete_table('formplayer_xform')
-
 
     models = {
         'formplayer.xform': {
